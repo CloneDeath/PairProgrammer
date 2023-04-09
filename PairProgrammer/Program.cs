3456381527368f593551ffe42431b9fa892000b1
Nicholas Rodine <halfofastaple@gmail.com>
2023-04-09 06:37:43 -0500

    More improvements.

--- PairProgrammer/Program.cs
+++ PairProgrammer/Program.cs
@@ -4,36 +4,33 @@ using Newtonsoft.Json;
 
 namespace PairProgrammer;
 
-public static class Program
-{
-	public static async Task Main(string[] args)
-	{
-		string apiKey = "your-api-key";
+public static class Program {
+	public static async Task Main(string[] args) {
+		var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")
+					 ?? throw new InvalidOperationException("The environment variable 'OPENAI_API_KEY' is not set.");
 
 		var chatGptApi = new ChatGptApi(apiKey);
 		var commandExecutor = new CommandExecutor(args[0]);
 
-		while (true)
-		{
+		while (true) {
 			Console.WriteLine("Enter your message:");
-			string input = Console.ReadLine();
+			var input = Console.ReadLine();
 
 			var prompt = "As an AI language model, you will help in pair programming for a software project. "
-						 + "You will be given commands in JSON format like `{\"access\": \"filename.txt\"}` to access a file, "
-						 + "or `{\"list\":\"/\"}` to list all files in a directory. "
+						 + "You will be able to give commands in a JSON format like `{\"access\": \"filename.txt\"}` to access a file, "
+						 + "or `{\"list\":\"./\"}` to list all files in a directory. "
 						 + "You will use your knowledge to provide useful responses and assistance in software development. "
-						 + "Now, please process the following user input: "
-						 + Environment.NewLine + input;
+						 + "Additionally, when writing code, please keep Robert Martin's Clean Code in mind at all times. "
+						 + "Now, please process the following user input:" + Environment.NewLine
+						 + input;
 			var response = await chatGptApi.GetChatGptResponseAsync(prompt);
 
-			try
-			{
+			try {
 				var command = JsonConvert.DeserializeObject<Command>(response) ?? throw new NullReferenceException("Input cannot be null or empty.");
-				string output = commandExecutor.ExecuteCommand(command);
+				var output = commandExecutor.ExecuteCommand(command);
 				Console.WriteLine(output);
 			}
-			catch (Exception ex)
-			{
+			catch (Exception ex) {
 				Console.WriteLine("An error occurred while executing the command: " + ex.Message);
 			}
 		}
